12e51e6ac73eea2bf723a8392e32f80494f60855
Amo Stepanyan <stamoern@gmail.com>
2024-08-22 11:04:38 +0300

    tv-app

--- playground/tv-app/main.js
+++ playground/tv-app/main.js
@@ -1,3 +1,3 @@
 // const script = [...document.querySelectorAll('script')].map((script => script.src)).join('<br>');
 
-document.body.innerHTML = `<h1>${window.appConfig}</h1>`;
+document.body.innerHTML = `<h1><pre>${JSON.stringify(window.appConfig)}</pre></h1>`;
